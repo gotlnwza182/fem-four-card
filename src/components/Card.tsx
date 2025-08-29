335da08946ad689ba37b77f5f87ebc12fbc82a6a
gotlnwza182 <6131501073@lamdaun.mfu.ac.th>
2025-08-29 17:36:43 +0700

    edited color variable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,9 +11,10 @@ const Card = ({
   color: string;
   grid?: string;
 }) => {
+  const borderColor = `border-${color}`;
   return (
     <div
-      className={`p-8 w-[314px] lg:w-[350px] h-[250px] bg-white rounded-lg border-t-4 border-${color} shadow-[0px_15px_30px_-11px_rgba(131,166,210,50)] ${grid}`}
+      className={`p-8 w-[314px] lg:w-[350px] h-[250px] bg-white rounded-lg border-t-4 ${borderColor} shadow-[0px_15px_30px_-11px_rgba(131,166,210,50)] ${grid}`}
     >
       <h1 className="text-preset-3 text-grey-500">{title}</h1>
       <p className="text-preset-5 text-grey-400">{description}</p>
